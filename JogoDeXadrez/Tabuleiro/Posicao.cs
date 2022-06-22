abbbb1cd94297259611b82532753f07be2dbb107
kiyohider <natanaeltoazza@hotmail.com.br>
2022-06-22 19:10:52 -0300

    tabuleiro implementação
    classes:
    Cor
    Peça
    Tabuleiro

--- JogoDeXadrez/Tabuleiro/Posicao.cs
+++ JogoDeXadrez/Tabuleiro/Posicao.cs
@@ -3,13 +3,13 @@ namespace Tabuleiro
 {
     class Posicao
     {
-        public int Linha { get; set; }
-        public int Coluna { get; set; }
+        public int linha { get; set; }
+        public int coluna { get; set; }
 
         public Posicao(int linha, int coluna)
         {
-            Linha = linha;
-            Coluna = coluna;
+            this.linha = linha;
+            this.coluna = coluna;
         }
 
         public override string ToString()
---
+++ JogoDeXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int numMovimento { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.numMovimento = 0;
+        }
+    }
+}
