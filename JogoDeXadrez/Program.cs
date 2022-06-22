dd943e8735fbe66da45130891b779b97f32d5447
kiyohider <natanaeltoazza@hotmail.com.br>
2022-06-22 18:57:49 -0300

    classe posição

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using Tabuleiro;
+
+namespace JogoDeXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+        }
+
+
+    }
+}
