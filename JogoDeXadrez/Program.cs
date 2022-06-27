f4bcf50de93978117cb70eb3ddde98ad71dfa722
kiyohider <natanaeltoazza@hotmail.com.br>
2022-06-27 19:08:50 -0300

    posições tabuleiro de xadrez

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -8,26 +8,35 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-                tabuleiro.colocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
-                tabuleiro.colocarPeca(new Cavalo(Cor.Preta, tabuleiro), new Posicao(0, 1));
-                tabuleiro.colocarPeca(new Bispo(Cor.Preta, tabuleiro), new Posicao(0, 2));
-                tabuleiro.colocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 3));
-                tabuleiro.colocarPeca(new Dama(Cor.Preta, tabuleiro), new Posicao(0, 4));
-                tabuleiro.colocarPeca(new Bispo(Cor.Preta, tabuleiro), new Posicao(0, 5));
-                tabuleiro.colocarPeca(new Cavalo(Cor.Preta, tabuleiro), new Posicao(0, 6));
-                tabuleiro.colocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 7));
 
 
-                Tela.imprimirTabuleiro(tabuleiro);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('a', 8);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
+
+
+            //try
+            //{
+            //    Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
+            //    tabuleiro.colocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+            //    tabuleiro.colocarPeca(new Cavalo(Cor.Preta, tabuleiro), new Posicao(0, 1));
+            //    tabuleiro.colocarPeca(new Bispo(Cor.Preta, tabuleiro), new Posicao(0, 2));
+            //    tabuleiro.colocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 3));
+            //    tabuleiro.colocarPeca(new Dama(Cor.Preta, tabuleiro), new Posicao(0, 4));
+            //    tabuleiro.colocarPeca(new Bispo(Cor.Preta, tabuleiro), new Posicao(0, 5));
+            //    tabuleiro.colocarPeca(new Cavalo(Cor.Preta, tabuleiro), new Posicao(0, 6));
+            //    tabuleiro.colocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 7));
+
+
+            //    Tela.imprimirTabuleiro(tabuleiro);
+            //}
+            //catch(TabuleiroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
 
 
 
