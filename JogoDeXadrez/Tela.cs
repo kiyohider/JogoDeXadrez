2031b75e339c0ed9170df96448775d87b661e634
kiyohider <natanaeltoazza@hotmail.com.br>
2022-06-27 19:22:00 -0300

    molhorado impressão tabuleiro
    diferenciar as cores

--- JogoDeXadrez/Tela.cs
+++ JogoDeXadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace JogoDeXadrez
         {
             for (int i=0; i<tabuleiro.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tabuleiro.colunas; j++)
                 {
                     if(tabuleiro.peca(i,j) == null)
@@ -16,13 +17,29 @@ namespace JogoDeXadrez
                         Console.Write("- ");
                     }
                     else 
-                    { 
-                    Console.Write(tabuleiro.peca(i,j) + " ");
+                    {
+                        imprimirPeca(tabuleiro.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.Write("  a b c d e f g h");
+        }
 
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
 
     }
